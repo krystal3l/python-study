85df032a9ea7804756db2c2b88f3c2ddc2763c61
krystal <8uartz@gmail.com>
2019-10-03 23:03:40 +0900

    format 추가

--- section02-1.py
+++ section02-1.py
@@ -23,3 +23,19 @@ print('niceman','google.com',sep='@')
 print('welcome To', end='')
 print(' the black', end ='')
 print (' piano notes')
+
+
+#format 사용
+print('{} and {}'.format('You','Me'))
+print("{0} and {1} and {0}".format('You','Me'))
+print("{a} are {b}".format(a='You', b='Me'))
+
+print("%s's favorite number is %d" % ('Eunki',7)) # %s : 문자, %d : 정수 , %f : 실수(소수점)
+
+print("Test1 : %5d, Price: %4.2f" % (776, 6534.123))
+print("Test1 : {0: 5d}, Price : {1:4.2f}".format(776,6534.123))
+print("Test1 : {a:5d}, Price : {b:4.2f}".format(a=776,b=6534.123))
+
+print("'you'")
+print('\'you\'')
+print('\t test')
